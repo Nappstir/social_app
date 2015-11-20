ca78103dd0493108d9486037e056d3d18ab203bd
Nappstir <trsiebenhaar@gmail.com>
2015-11-20 16:17:35 -0700

    Add test to check that emails are saved downcase

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -56,6 +56,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email should be saved as lowercase" do
+    mixed_case_email = "TEst@GmAiL.COM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   # User validation test for password
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
