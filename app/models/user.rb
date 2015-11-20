60643d2b737fdd6ba3a8cc61f3d2997d9a229aa9
Nappstir <trsiebenhaar@gmail.com>
2015-11-20 16:23:16 -0700

    Add code to VALID_EMAIL_REGEX variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
 
   # Email Validations
-  VALID_EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   # Password Validations
