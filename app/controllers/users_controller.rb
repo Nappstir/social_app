c5ca36397b7a589ce1ce5cd497ec64c075177710
Travis Siebenhaar <trsiebenhaar@gmail.com>
2015-12-10 00:25:09 -0700

    Add edit form & tests, add allow_nil to password validations, add user before filter(logged_in_user)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -7,6 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
 
   def create
     @user = User.new(user_params)
@@ -19,11 +22,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+# BEFORE FILTERS
+
+# Confirms user is logged in
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_path
     end
 
 end
