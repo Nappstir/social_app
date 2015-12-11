b3bd851c88f18c6d5f1eb12a4edb8923d9e3b57d
Travis Siebenhaar <trsiebenhaar@gmail.com>
2015-12-10 21:32:32 -0700

    Add user index route, css, html display. Add test for index route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :index]
   before_action :correct_user, only: [:edit, :update]
 
   def new
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
 
   def create
     @user = User.new(user_params)
