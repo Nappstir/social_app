60598934b8473cd94d6f3b168c9971fe47b61e73
Travis Siebenhaar <trsiebenhaar@gmail.com>
2015-12-18 15:34:47 -0700

    Add post/user association & dependent destroy, add default_scope desc order posts, add tests for most recent post & dependent destroy.

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,8 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:ExampleUser)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    # build returns obj in memory w/out modifying db
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -24,6 +25,10 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should not exceed 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 
 end
