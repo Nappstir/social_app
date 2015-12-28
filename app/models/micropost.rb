03a475f7637e93bf76be17b3aeb779c189b36a2c
Travis Siebenhaar <trsiebenhaar@gmail.com>
2015-12-28 14:05:26 -0700

    Add pictures to posts, add sizing validations(server/client side), add imagemagick for resizing images.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,16 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+  mount_uploader :picture, PictureUploader
+
+  private
+
+  # Validates the size of the uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+
 end
