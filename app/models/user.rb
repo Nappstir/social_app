44cc03da5c78494d1dcf9e2faaa613ab0a4056ad
Travis Siebenhaar <trsiebenhaar@gmail.com>
2015-12-30 19:25:50 -0700

    Add user feed tests, Add sql subselect for feed method, finish display of posts from all users being followed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,12 +72,15 @@ class User < ActiveRecord::Base
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
+  # Returns true if password has expired
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
 
+  # Returns a user's status feed
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # Follow a user
