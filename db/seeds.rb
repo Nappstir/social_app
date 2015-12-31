d0bbe336a304033f96c5f316be41acf802b35765
Travis Siebenhaar <trsiebenhaar@gmail.com>
2015-12-30 21:50:37 -0700

    Make adjustments to seed file, Add region key to fog config.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,20 @@
 
 User.create(name: "Test Testing", email: "testing@test.com", password: "testing", password_confirmation: "testing", admin: true, activated: true)
 
-99.times do
+50.times do
   User.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "testing", password_confirmation: "testing", activated: true)
 end
 
 # take the first 6 users from database & create microposts
 users = User.order(:created_at).take(6)
-50.times do
+15.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
 
 users = User.all
 user = User.first
-following = users[2..50]
-followers = users[3..40]
+following = users[20..45]
+followers = users[10..50]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
